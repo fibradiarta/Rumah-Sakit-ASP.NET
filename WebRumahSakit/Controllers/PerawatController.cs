242aebe22785dfb20fe94ea2fb58781c2340ed46
DESKTOP-R776IKC\fibradiarta <fibradiarta@gmail.com>
2018-09-17 19:48:32 +0700

    first commit

---
+++ WebRumahSakit/Controllers/PerawatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRumahSakit.Controllers
+{
+    public class PerawatController : Controller
+    {
+        // GET: Perawat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebRumahSakit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebRumahSakit.MvcApplication" Language="C#" %>
