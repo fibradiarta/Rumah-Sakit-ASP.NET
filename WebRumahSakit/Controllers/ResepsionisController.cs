8cfff289529464652e74b661637c01557efa019c
Tsani Elvia Nita <elvianitatsani@yahoo.co.id>
2018-09-18 09:28:17 +0700

    Resepsionis

---
+++ WebRumahSakit/Controllers/ResepsionisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRumahSakit.Controllers
+{
+    public class ResepsionisController : Controller
+    {
+        // GET: Resepsionis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
