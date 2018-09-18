4636ffb44c3c3da176e1b1d7e6a1c22bcc41b930
Tsani Elvia Nita <elvianitatsani@yahoo.co.id>
2018-09-18 14:23:24 +0700

    Kasir

---
+++ WebRumahSakit/Controllers/TransaksiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRumahSakit.Controllers
+{
+    public class TransaksiController : Controller
+    {
+        // GET: Transaksi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
